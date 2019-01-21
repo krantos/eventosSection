ea61671f79c92ccb614e6fec57dfdbad81ad8234
marcos_riveros <marcosriverosk@gmail.com>
2019-01-20 21:07:10 -0300

    casi listo

--- src/js/imageCanvasLoader.js
+++ src/js/imageCanvasLoader.js
@@ -1,280 +1,197 @@
 import React, { Component } from "react";
-import {hot} from "react-hot-loader";
+import { hot } from "react-hot-loader";
 import "../style/imageCanvasLoader.css";
 
 
-const userWidth = 1024;
-const userHeight = 768;
+const userWidth = 650;
+const userHeight = 430;
 
 class ImageCanvasLoader extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: [],
-      resized: false,
-      status: false,
-    };
-    this.loadImages = this.loadImages.bind(this);
-    this.loadIntoCanvas = this.loadIntoCanvas.bind(this);
-    this.removeImage = this.removeImage.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			images: [],
+			resized: false,
+			status: false,
+		};
+		this.loadImages = this.loadImages.bind(this);
+		this.loadIntoPreview = this.loadIntoPreview.bind(this);
+		this.removeImage = this.removeImage.bind(this);
+		this.addOneMoreImage = this.addOneMoreImage.bind(this);
+	}
 
+	loadImages(e) {
+		this.loadWithReaderImages(e.target.files, true);
+	}
 
-  componentDidMount() {}
+	addOneMoreImage(e) {
+		this.loadWithReaderImages(e.target.files, false);
+	}
 
-  loadImages(e) {
-    this.loadWithReaderImages(e.target.files);
-  }
+	loadWithReaderImages(images, multiple) {
+		this.setState({
+			resized: false,
+		});
+		let promises = [...images].map(i => {
+			const reader = new FileReader();
+			const prom = new Promise((resolve, reject) => {
+				reader.onload = function () {
+					resolve(reader.result);
+				}
+			});
+			reader.readAsDataURL(i);
+			return prom;
+		});
+		Promise.all(promises).then(loadedImage => {
+			this.resizeImage(loadedImage, multiple);
+			this.setState({
+				status: true
+			})
+		});
+	}
 
-  loadWithReaderImages(images) {
-    let promises = [...images].map(i => {
-      const reader = new FileReader();
-      const prom = new Promise((resolve, reject) => {
-        reader.onload = function(){
-          resolve(reader.result);
-        }
-      });
-      reader.readAsDataURL(i);
-      return prom;
-    });
-    Promise.all(promises).then(loadedImage => {
-      this.resizeImage(loadedImage);
-      this.setState({
-        status: true
-      })
-    });
-  }
+	resizeImage(imageData, multiple) {
 
-  resizeImage(imageData) {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    let promises = imageData.map(i => {
+		const canvas = document.createElement('canvas');
+		const ctx = canvas.getContext('2d');
+		let promises = imageData.map(i => {
 
-      const imgHelper = new Image();
+			const imgHelper = new Image();
 
-      const prom = new Promise(resolve => {
+			const prom = new Promise(resolve => {
 
-          imgHelper.onload = function() {
-          canvas.width = userWidth;
-          canvas.height = userHeight;
-          ctx.fillStyle = '#fff'; /// set white fill style
-          ctx.fillRect(0, 0, canvas.width, canvas.height);
-          const nw = imgHelper.naturalWidth;
-          const nh = imgHelper.naturalHeight;
-          let h = canvas.height;
-          let w = canvas.width;
-          if (nw / nh < w / h) {
-            w = h * nw / nh;
-          } else {
-            h = w * nh / nw;
-          }
-          const px = (canvas.width - w) / 2;
-          const py = (canvas.height - h) / 2;
-          ctx.drawImage(imgHelper, px, py, w, h);
-          let data = canvas.toDataURL('image/jpeg', 0.85);
-          resolve(data);
-        }
-      });
+				imgHelper.onload = function () {
+					canvas.width = userWidth;
+					canvas.height = userHeight;
+					ctx.fillStyle = '#fff'; /// set white fill style
+					ctx.fillRect(0, 0, canvas.width, canvas.height);
+					const nw = imgHelper.naturalWidth;
+					const nh = imgHelper.naturalHeight;
+					let h = canvas.height;
+					let w = canvas.width;
+					if (nw / nh < w / h) {
+						w = h * nw / nh;
+					} else {
+						h = w * nh / nw;
+					}
+					canvas.width = w;
+					canvas.height = h;
+					const px = (canvas.width - w) / 2;
+					const py = (canvas.height - h) / 2;
+					ctx.drawImage(imgHelper, px, py, w, h);
+					let data = canvas.toDataURL('image/jpeg', 0.88);
+					resolve(data);
+				}
+			});
 
-      imgHelper.src = i;
-      return prom;
-    });
+			imgHelper.src = i;
+			return prom;
+		});
 
-    Promise.all(promises).then(imageResized => {
-      this.setState({
-        images: imageResized,
-        resized: true,
-        status: true,
-      });
-    })
-  }
+		if (multiple) {
+			Promise.all(promises).then(imageResized => {
+				this.setState({
+					images: imageResized,
+					resized: true,
+					status: true,
+				});
+			})
+		} else {
+			Promise.all(promises).then(imageResized => {
+				const newImageArray = [...this.state.images, imageResized];
+				this.setState({
+					images: newImageArray,
+					resized: true,
+					status: true,
+				});
+			})
+		}
 
-  loadIntoCanvas(ev) {
-    const imgData = new Image();
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    imgData.src = ev.target.src;
-    canvas.width = userWidth;
-    canvas.height = userHeight;
-    ctx.drawImage(imgData, 0, 0, 645, 430);
-    console.log(ev.target.name);
-    this.switchSelected(ev.target);
-  }
+	}
 
-  switchSelected(imgTarget) {
-    const previousImg = document.querySelector('.selected');
-    if(previousImg) {
-      previousImg.classList.toggle('selected');
-      imgTarget.classList.toggle('selected');
-    } else {
-      imgTarget.classList.toggle('selected');
-    }
-    
-  }
+	loadIntoPreview(ev) {
+		const imgPreview = document.getElementById('previewOverCanvas');
+		imgPreview.src = ev.target.src;
+	}
 
-  cleanCanvas() {
+	switchSelected(imgTarget) {
+		const previousImg = document.querySelector('.selected');
+		if (previousImg) {
+			previousImg.classList.toggle('selected');
+			imgTarget.classList.toggle('selected');
+		} else {
+			imgTarget.classList.toggle('selected');
+		}
+	}
 
-  }
+	removeImage(ev) {
+		console.log(ev.target.name);
+		let images = this.state.images;
+		images.splice(ev.target.name, 1);
+		this.setState({
+			images: images
+		});
+	}
 
-  removeImage(ev) {
-    console.log(ev.target.name);
-    let images = this.state.images;
-    images.splice(ev.target.name, 1);
-    this.cleanCanvas();
-    this.setState({
-      images: images
-    });
-  }
+	render() {
+		const { status, resized, images } = this.state;
+		if (!status && !resized) {
 
-  render() {
-    const {status, resized, images} = this.state;
-    if( !status && !resized ) {
+			return (
+				<div className="App">
+					<input id="inputi" type="file" multiple name="images" accept="image/*" onChange={this.loadImages} />
+					<canvas id="canvas">
+					</canvas>
+				</div>
+			);
 
-      return(
-        <div className="App">
-          <input id="inputi" type="file" multiple name="images" accept="image/*" onChange={this.loadImages}/>
-          <canvas id="canvas">
-          </canvas>
-        </div>
-      );
+		}
 
-    } 
+		if (status && !resized) {
+			return (
+				<div className="waiting">
+				...Loading
+				</div>
+			)
+		}
 
-    if(status && !resized) {
-      return (
-        <div className="waiting">
-        </div>
-      )
-    }
-    
-    if(status && resized) {
-      return(
-        <div className="row">
-        <div className="col-md-6">
-          <canvas  id="canvas">
-          </canvas>
-        </div>
-        <div className="col-md-4 imgsPreviews">
-          <div className="row">
-          {images.map((image,index) => {
-            return (
-              <div className='col-sm-4 imgContainer' key={index} >
-                <img 
-                  className="img-fluid" 
-                  name={index} 
-                  src={image} 
-                  onLoad={this.imageElement} 
-                  onClick={this.loadIntoCanvas}/>
-                <button 
-                  type="button" 
-                  className="btn btn-danger" 
-                  name={index} 
-                  onClick={this.removeImage}> &#10008; </button>
-              </div>
-            );}
-          )}
-          </div>
-        </div>
-        </div>
-      );
-
-    } 
-  }
+		if (status && resized) {
+			return (
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6">
+							<img src="" alt="" id="previewOverCanvas"/>
+						</div>
+					</div>
+					<div className="row">
+								{images.map((image, index) => {
+									return (
+										<div className='col-2 imgContainer' key={index} >
+											<img
+												className="img-fluid"
+												name={index}
+												src={image}
+												onLoad={this.imageElement}
+												onClick={this.loadIntoPreview} />
+											<button
+												type="button"
+												className="btn btn-danger"
+												name={index}
+												onClick={this.removeImage}> &#10008; </button>
+										</div>
+									);
+								}
+								)}
+								<div className='col-2 imgContainer upload-btn-wrapper'>
+									<button className="btn btn-primary">&#10010;</button>
+									<input id="oneMoreInput" type="file" name="images" accept="image/*" onChange={this.addOneMoreImage} />
+								</div>
+							</div>
+						</div>
+			);
+		}
+	}
 }
-
-
-
-// transformImages(images) {   
-//         let promiseReader = [];
-//     for(let i of images) {
-//         let imageHelper = new Image();
-//         let finalData = [];
-//         const canvas = document.getElementById('canvas');
-//         const ctx = canvas.getContext('2d');
-//         const reader = new FileReader();
-//         const rPromise = new Promise(resolve => {    
-//           reader.onload = function() {
-//             imageHelper.src = this.result;
-//             canvas.width = 645;
-//             canvas.height = 430;
-//             ctx.fillStyle = '#fff'; /// set white fill style
-//             ctx.fillRect(0, 0, canvas.width, canvas.height);
-//             const nw = imageHelper.naturalWidth;
-//             const nh = imageHelper.naturalHeight;
-//             let h = canvas.height;
-//             let w = canvas.width;
-//             if (nw / nh < w / h) {
-//               w = h * nw / nh;
-//             } else {
-//               h = w * nh / nw;
-//             }
-//             const px = (canvas.width - w) / 2;
-//             const py = (canvas.height - h) / 2;
-//             ctx.drawImage(imageHelper, px, py, w, h);
-//             let data = canvas.toDataURL('image/jpeg', 0.85);
-//             // console.log("data ", data);
-//             resolve(data);
-//           }
-//         });
-//         promiseReader = [...promiseReader, rPromise];
-//         reader.readAsDataURL(i);
-//       }
-
-//       Promise.all(promiseReader).then(data => 
-//         finalData = [...finalData, data]
-//       );
-//   }
-
-// function CreateImageElement({image}) {
-//   console.log(image);
-//   return (
-    
-//   );
-// }
-
-// function LoadPreview (imagesToTransform) {
-
-//     const resized = document.getElementById('resized');
-//     const canvas = document.getElementById('canvas');
-//     let ctx = canvas.getContext('2d');
-//     let finalData = [];
-
-//     for (const i of imagesToTransform) { 
-//       const reader = new FileReader();
-//       reader.addEventListener('load', function () {
-//         let image = new Image();
-
-//         image.onload = function () {
-
-//           canvas.width = 645;
-//           canvas.height = 430;
-//           ctx.fillStyle = '#fff'; /// set white fill style
-//           ctx.fillRect(0, 0, canvas.width, canvas.height);
-//           const nw = image.naturalWidth;
-//           const nh = image.naturalHeight;
-//           let h = canvas.height;
-//           let w = canvas.width;
-//           if (nw / nh < w / h) {
-//             w = h * nw / nh;
-//           } else {
-//             h = w * nh / nw;
-//           }
-//           // canvas.width = w;
-//           // canvas.height = h;
-//           const px = (canvas.width - w) / 2;
-//           const py = (canvas.height - h) / 2;
-//           ctx.drawImage(image, px, py, w, h);
-//           let data = canvas.toDataURL('image/jpeg', 0.85);
-//           finalData = [...finalData, data];
-//         };
-
-//         image.src = this.result;
-
-//       }, false);
-
-//       reader.readAsDataURL(i);
-//     }
-// }
 
 export default hot(module)(ImageCanvasLoader);
